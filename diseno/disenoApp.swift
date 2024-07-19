074f8798c9289ec7d57ebdb6ea654d45ac51fc04
Miguel Martinez <miguel.martinez.angel.salazar@gmail.com>
2024-07-18 23:35:37 -0300

    Initial Commit

---
+++ diseno/disenoApp.swift
@@ -0,0 +1,17 @@
+//
+//  disenoApp.swift
+//  diseno
+//
+//  Created by Miguel Martinez on 18/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct disenoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
